6053afdb15a48885a492009609368111da30033f
deodat04 <deodatadandedjan2000@gmail.com>
2024-10-18 15:00:16 +0100

    update readme and metriques

--- metriques.py
+++ metriques.py
@@ -20,14 +20,13 @@ def metriques_csv(gold_standard, result_file):
 
     exactitude = nombre_corrects / total_lignes
     print(f"Exactitude globale : {exactitude * 100:.2f}%")
-    print(nombre_corrects)
     return nombre_corrects
 
 
 def analyse_metrique_negatif(gold_standard, result_file):
     # Classe réelle A (avis négatif), classe réelle ¬A (avis positif ou neutre)
     A = 'Négatif'
-    NA = ['Positif', 'Neutre']  # Positif et Neutre sont considérés comme ¬A
+    NA = ['Positif', 'Neutre']
 
     VP = 0  # Vrai Positif: Négatif prédit et réel
     FN = 0  # Faux Négatif: Négatif réel mais non prédit
@@ -51,17 +50,95 @@ def analyse_metrique_negatif(gold_standard, result_file):
             elif row_gold in NA and row_result in NA:
                 VN += 1  # Correctement classé comme non négatif
 
-    # Affichage des résultats
     print(f"Vrai Positif (VP) : {VP}")
     print(f"Vrai Négatif (VN) : {VN}")
     print(f"Faux Positif (FP) : {FP}")
     print(f"Faux Négatif (FN) : {FN}")
 
-    # Calcul de l'exactitude avec la formule proposée
     exactitude_negatif = (VP + VN) / (VP + VN + FP + FN)
+    precision_negatif = VP / (VP + FP)
     print(f"Exactitude sur les avis négatifs : {exactitude_negatif * 100:.2f}%")
+    print(f"Précision sur les avis négatifs : {precision_negatif * 100:.2f}%")
     return VP, VN, FP, FN
 
+
+def analyse_metrique_positif(gold_standard, result_file):
+    # Classe réelle A (avis positif), classe réelle ¬A (avis negatif ou neutre)
+    A = 'Positif'
+    NA = ['Négatif', 'Neutre']
+
+    VP = 0  # Vrai Positif: Positif prédit et réel
+    FN = 0  # Faux Négatif: Positif réel mais non prédit
+    FP = 0  # Faux Positif: Positif prédit mais non réel
+    VN = 0  # Vrai Négatif: Non Positif prédit et non positif réel
+
+    with open(gold_standard, mode='r', encoding='utf-8') as gold, open(result_file, mode='r', encoding='utf-8') as res:
+        reader1 = csv.DictReader(gold)
+        reader2 = csv.DictReader(res)
+
+        for row1, row2 in zip(reader1, reader2):
+            row_gold = row1['reviews.doRecommend']
+            row_result = row2['result']
+
+            if row_gold == A and row_result == A:
+                VP += 1  
+            elif row_gold == A and row_result in NA:
+                FN += 1 
+            elif row_gold in NA and row_result == A:
+                FP += 1  
+            elif row_gold in NA and row_result in NA:
+                VN += 1  
+
+    print(f"Vrai Positif (VP) : {VP}")
+    print(f"Vrai Négatif (VN) : {VN}")
+    print(f"Faux Positif (FP) : {FP}")
+    print(f"Faux Négatif (FN) : {FN}")
+
+    exactitude_positif = (VP + VN) / (VP + VN + FP + FN)
+    precision_positif = VP / (VP + FP)
+    print(f"Exactitude sur les avis positifs : {exactitude_positif * 100:.2f}%")
+    print(f"Précision sur les avis positifs : {precision_positif * 100:.2f}%")
+    return VP, VN, FP, FN
+
+
+
+def analyse_metrique_neutre(gold_standard, result_file):
+    # Classe réelle A (avis neutre), classe réelle ¬A (avis positif ou négatif)
+    A = 'Neutre'
+    NA = ['Positif', 'Négatif']
+
+    VP = 0  # Vrai Positif: Neutre prédit et réel
+    FN = 0  # Faux Négatif: Neutre réel mais non prédit
+    FP = 0  # Faux Positif: Neutre prédit mais non réel
+    VN = 0  # Vrai Négatif: Non Neutre prédit et non neutre réel
+
+    with open(gold_standard, mode='r', encoding='utf-8') as gold, open(result_file, mode='r', encoding='utf-8') as res:
+        reader1 = csv.DictReader(gold)
+        reader2 = csv.DictReader(res)
+
+        for row1, row2 in zip(reader1, reader2):
+            row_gold = row1['reviews.doRecommend']
+            row_result = row2['result']
+
+            if row_gold == A and row_result == A:
+                VP += 1  
+            elif row_gold == A and row_result in NA:
+                FN += 1 
+            elif row_gold in NA and row_result == A:
+                FP += 1 
+            elif row_gold in NA and row_result in NA:
+                VN += 1 
+
+    print(f"Vrai Positif (VP) : {VP}")
+    print(f"Vrai Négatif (VN) : {VN}")
+    print(f"Faux Positif (FP) : {FP}")
+    print(f"Faux Négatif (FN) : {FN}")
+
+    exactitude_neutre = (VP + VN) / (VP + VN + FP + FN)
+    precision_neutre = VP / (VP + FP)
+    print(f"Exactitude sur les avis neutres : {exactitude_neutre * 100:.2f}%")
+    print(f"Précision sur les avis neutres : {precision_neutre * 100:.2f}%")
+    return VP, VN, FP, FN
 
 # Fichiers d'entrée
 gold = 'gold_standard.csv'
@@ -72,3 +149,9 @@ metriques_csv(gold, res)
 
 # Analyser les métriques spécifiques aux avis négatifs
 analyse_metrique_negatif(gold, res)
+print('\n')
+# Analyser les métriques spécifiques aux avis positifs
+analyse_metrique_positif(gold, res)
+print('\n')
+# Analyser les métriques spécifiques aux avis neutre
+analyse_metrique_neutre(gold, res)
--- readme.md
+++ readme.md
@@ -1,22 +1,39 @@
-#Analyse de sentiments
-#Execution du programme d' approche naive
+# Analyse de sentiments
+# Execution du programme | Approche naive
 
 
 ## 1. Ouvrir  le terminal 
-   `ctrl+alt+t`
+`ctrl+alt+t`
 
 ## 2. Se connecter à l'environnement virtuel tp_python
-  `source /usr/bin/tp_python_virtualenv`
+`source /usr/bin/tp_python_virtualenv`
 
-## 3. Executer le programme
-   `python naive_feeling.py`
+## 3. Naviguer vers le répertoires du projet
+`cd repertoire/du/projet`
 
-## 4. Informations nécessaire suite à l'execution du programme
-    - après exécution du programme suivez les instructions si
+## 4. Executer le programme
+`python naive_feeling.py`
+
+## 5. Informations nécessaire suite à l'execution du programme
+- après exécution du programme suivez les instructions si
 vous souhaitez visualiser les synonymes générés par Wordnet 
 pour l'analyse de corpus
-    - En ouvrant le répertoire du projet, un ficher *results.csv
-est généré et afficher les résultats de chaque avis
+- (taper **1** pour accéder à la liste des synonymes et **2** pour ne pas y accéder)
+- En ouvrant le répertoire du projet, un ficher **results.csv**
+est généré et affiche les résultats de chaque avis.
 
-## 5. Désactiver l'environnement virtuel
-    `deactivate`
+## 6. Désactiver l'environnement virtuel
+`deactivate`
+
+## 7. Lien vers dépôt github du projet
+[Sentiment-Analysis sur GitHub](https://github.com/deodat04/sentiment-analysis.git)
+
+
+## 8. Informations sur certains fichiers et dossier du projet
+- metriques.py est le programme permettant de calculer l'exactitude de cette approche
+- assets contient les images utilisées dans le rapport qui présentent les différentes approches
+- ElectronicsReviews.csv représente le corpus initial et gold_standard.csv le gold standard
+
+
+- https://github.com/nltk/nltk/blob/develop/nltk/sentiment/vader.py
+- https://stackoverflow.com/questions/40325980/how-is-the-vader-compound-polarity-score-calculated-in-python-nltk
